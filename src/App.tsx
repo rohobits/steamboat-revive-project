2ef59a63894fb9572b22d0397f5b66bc22c14bc9
rohobits <fostermartin67@gmail.com>
2025-04-26 23:07:15 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BikeService from "./pages/BikeService";
@@ -14,8 +15,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* set basename to your GH Pages subpath */}
       <BrowserRouter basename="/steamboat-revive-project">
+        {/* Scroll back to top on every route change */}
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/bike-service" element={<BikeService />} />
