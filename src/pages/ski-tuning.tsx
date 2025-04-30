d1535319af94693e902d49bdf2494173dadd73b4
rohobits <fostermartin67@gmail.com>
2025-04-30 16:12:08 -0600

    Update ski-tuning.tsx

--- src/pages/ski-tuning.tsx
+++ src/pages/ski-tuning.tsx
@@ -25,6 +25,7 @@ export default function SkiTuningPage() {
               loop
               muted
               playsInline
+              preload="auto"
             >
               <source
                 src={`${BASE_URL}videos/ski-tune-loop.mp4`}
