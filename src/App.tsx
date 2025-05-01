061acf37e92f930f61d25d691ab408bab65e20f9
rohobits <fostermartin67@gmail.com>
2025-04-30 22:00:43 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 
 import Index from "./pages/Index";
 import BikeService from "./pages/BikeService";
 import EventsPage from "./pages/EventsPage";
 import BootFittingPage from "./pages/Boot-fitting";
-import SkiTuningPage from "./pages/ski-tuning";    // ← import your new page
-import NotFound from "./pages/NotFound";
+import SkiTuningPage from "./pages/ski-tuning";
 import ShopPage from "./pages/ShopPage";
+import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <BrowserRouter basename="/steamboat-revive-project">
+  <Router basename="/steamboat-revive-project">
     <ScrollToTop />
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/bike-service" element={<BikeService />} />
       <Route path="/events" element={<EventsPage />} />
-      <Route path="/Boot-fitting" element={<BootFittingPage />} />
-      <Route path="/ski-tuning" element={<SkiTuningPage />} />    {/* ← new */}
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+      <Route path="/boot-fitting" element={<BootFittingPage />} />
+      <Route path="/ski-tuning" element={<SkiTuningPage />} />
+      <Route path="/shop" element={<ShopPage />} />
+      {/* catch-all must go last */}
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
