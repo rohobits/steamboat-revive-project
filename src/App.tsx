51abef51fd876c4a4f379224171e890a7c2bd008
rohobits <fostermartin67@gmail.com>
2025-04-30 22:06:50 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 
-import Index from "./pages/Index";
-import BikeService from "./pages/BikeService";
-import EventsPage from "./pages/EventsPage";
+import Index           from "./pages/Index";
+import BikeService     from "./pages/BikeService";
+import EventsPage      from "./pages/EventsPage";
 import BootFittingPage from "./pages/Boot-fitting";
-import SkiTuningPage from "./pages/ski-tuning";
-import ShopPage from "./pages/ShopPage";
-import NotFound from "./pages/NotFound";
+import SkiTuningPage   from "./pages/ski-tuning";
+import ShopPage        from "./pages/ShopPage";
+import NotFound        from "./pages/NotFound";
 
 const App = () => (
-  <Router basename="/steamboat-revive-project">
+  <Router basename={import.meta.env.BASE_URL}>
     <ScrollToTop />
+
     <Routes>
+      {/* Home */}
       <Route path="/" element={<Index />} />
+
+      {/* Services */}
       <Route path="/bike-service" element={<BikeService />} />
-      <Route path="/events" element={<EventsPage />} />
+      <Route path="/events"       element={<EventsPage />} />
       <Route path="/boot-fitting" element={<BootFittingPage />} />
-      <Route path="/ski-tuning" element={<SkiTuningPage />} />
+      <Route path="/ski-tuning"   element={<SkiTuningPage />} />
+
+      {/* Shop */}
       <Route path="/shop" element={<ShopPage />} />
-      {/* catch-all must go last */}
+
+      {/* Catch-all for anything else */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
