a22d34df561c1a58a755a21adf6cf177e0221869
rohobits <fostermartin67@gmail.com>
2025-04-30 19:14:12 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import BikeService from "./pages/BikeService";
 import EventsPage from "./pages/EventsPage";
+import BootFittingPage from "./pages/Boot-fitting";
 import SkiTuningPage from "./pages/ski-tuning";    // ← import your new page
 import NotFound from "./pages/NotFound";
 
@@ -14,6 +15,7 @@ const App = () => (
       <Route path="/" element={<Index />} />
       <Route path="/bike-service" element={<BikeService />} />
       <Route path="/events" element={<EventsPage />} />
+      <Route path="/Boot-fitting" element={<BootFittingPage />} />
       <Route path="/ski-tuning" element={<SkiTuningPage />} />    {/* ← new */}
       {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
       <Route path="*" element={<NotFound />} />
