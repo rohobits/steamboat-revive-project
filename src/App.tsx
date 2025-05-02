c19f39c6af7893cf52629cc3096b6a1dd087da9c
rohobits <fostermartin67@gmail.com>
2025-05-01 21:39:49 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // src/App.tsx
 
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 
@@ -11,20 +12,20 @@ import SkiTuningPage from "./pages/ski-tuning";
 import ShopPage from "./pages/ShopPage";
 import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <BrowserRouter basename="/steamboat-revive-project">
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/bike-service" element={<BikeService />} />
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="/Boot-fitting" element={<BootFittingPage />} />
-      <Route path="/ski-tuning" element={<SkiTuningPage />} />
-      <Route path="/ShopPage" element={<ShopPage />} />  {/* ← Add this line */}
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-export default App;
+export default function App() {
+  return (
+    // No basename here — we want all Links and routes to be rooted at "/"
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/bike-service" element={<BikeService />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/Boot-fitting" element={<BootFittingPage />} />
+        <Route path="/ski-tuning" element={<SkiTuningPage />} />
+        <Route path="/ShopPage" element={<ShopPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
