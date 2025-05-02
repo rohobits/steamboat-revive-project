e6e067d661714a659c4caed7e769b6b07ce98379
rohobits <fostermartin67@gmail.com>
2025-05-01 21:48:07 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-// src/App.tsx
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 
 import Index from "./pages/Index";
 import BikeService from "./pages/BikeService";
-import EventsPage from "./pages/EventsPage";
 import BootFittingPage from "./pages/Boot-fitting";
 import SkiTuningPage from "./pages/ski-tuning";
 import ShopPage from "./pages/ShopPage";
+import EventsPage from "./pages/EventsPage";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    // No basename here — we want all Links and routes to be rooted at "/"
     <BrowserRouter>
       <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/bike-service" element={<BikeService />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/Boot-fitting" element={<BootFittingPage />} />
+        <Route path="/boot-fitting" element={<BootFittingPage />} />
         <Route path="/ski-tuning" element={<SkiTuningPage />} />
-        <Route path="/ShopPage" element={<ShopPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/events" element={<EventsPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
