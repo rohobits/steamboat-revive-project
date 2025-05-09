ae8ded9be599ebfa45d514bc0fcab2d8a5506613
rohobits <fostermartin67@gmail.com>
2025-05-08 20:42:57 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 // src/App.tsx
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import ScrollToTop from './components/ScrollToTop';
+
+import NavBar from '@/components/NavBar';
+import ScrollToTop from '@/components/ScrollToTop';
 
 // Page components
-import Index from './pages/Index';
-import BikeService from './pages/BikeService';
-import BikeRentalsPage from './pages/BikeRentalsPage';
-import BootFittingPage from './pages/Boot-fitting';
-import SkiTuningPage from './pages/ski-tuning';
-import ShopPage from './pages/ShopPage';
-import ShopBicyclesPage from './pages/ShopBicyclesPage';
-import EventsPage from './pages/EventsPage';
-import SkiRentalsPage from './pages/SkiRentalsPage';
-import SkiRentalsMountainPage from './pages/SkiRentalsMountainPage';
-import SkiRentalsDowntownPage from './pages/SkiRentalsDowntownPage';
-import NotFound from './pages/NotFound';
+import Index from '@/pages/Index';
+import BikeService from '@/pages/BikeService';
+import BikeRentalsPage from '@/pages/BikeRentalsPage';
+import BootFittingPage from '@/pages/Boot-fitting';
+import SkiTuningPage from '@/pages/ski-tuning';
+import ShopPage from '@/pages/ShopPage';
+import ShopBicyclesPage from '@/pages/ShopBicyclesPage';
+import EventsPage from '@/pages/EventsPage';
+import SkiRentalsPage from '@/pages/SkiRentalsPage';
+import SkiRentalsMountainPage from '@/pages/SkiRentalsMountainPage';
+import SkiRentalsDowntownPage from '@/pages/SkiRentalsDowntownPage';
+import NotFound from '@/pages/NotFound';
 
 export default function App() {
   return (
