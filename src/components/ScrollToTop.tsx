f0ebcb86cd83fadb7db35fa3c93f1da8f1c5cb97
rohobits <fostermartin67@gmail.com>
2025-05-08 21:56:02 -0600

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -7,18 +7,12 @@ export default function ScrollToTop() {
   const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    // If there's a hash (home-page anchor), smooth scroll to it:
     if (hash) {
-      const id = hash.replace("#", "");
-      const el = document.getElementById(id);
-      if (el) {
-        el.scrollIntoView({ behavior: "smooth" });
-        return;
-      }
+      const element = document.getElementById(hash.slice(1));
+      element?.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo(0, 0);
     }
-
-    // Otherwise, jump immediately to top of new page:
-    window.scrollTo(0, 0);
   }, [pathname, hash]);
 
   return null;
